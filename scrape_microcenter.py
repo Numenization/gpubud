397dde1cd8606de12a9d1e45a3189d8c97bba91d
njspence <nicholas.spencer5@gmail.com>
2025-03-02 19:10:11 -0500

    prettied up scraper

--- scrape_microcenter.py
+++ scrape_microcenter.py
@@ -1,5 +1,5 @@
-import json
 import requests
+import json
 from bs4 import BeautifulSoup
 
 def scrape():
@@ -11,18 +11,51 @@ def scrape():
     data = {}
     for item in items:
         link = item.find_next('a')
+        link2 = link.find_next('a')
+
+        parsed_name = parse_name(link['data-name'])
+
+        if parsed_name is None:
+            continue
+
         data[link['data-id']] = {
-            'brand': link['data-brand'],
+            'manufacturer': link['data-brand'],
             'name': link['data-name'],
-            'price': link['data-price']
+            'price': link['data-price'],
+            'brand': parsed_name['brand'],
+            'line': parsed_name['line'],
+            'model': parsed_name['model'],
+            'link': f'https://www.microcenter.com{link2["href"]}'
         }
 
     return data
 
+def parse_name(name):
+    tokens = name.split(' ')
+    data = {}
+
+    # get brand
+    data['brand'] = tokens[0]
+
+    # check for GeForce RTX cards
+    if tokens[1] == 'GeForce' and tokens[2] == 'RTX':
+        data['line'] = 'GeForce RTX'
+    # check for Radeon RX cards
+    elif tokens[1] == 'Radeon' and tokens[2] == 'RX':
+        data['line'] = 'Radeon RX'
+    else:
+        # we don't want any cards that arent RTX or RX
+        return None
+
+    # get model of card
+    data['model'] = tokens[3]
+
+    return data
+
+
 def main():
     data = scrape()
-    for k, v in data.items():
-        print(f'{v["name"]}: ${v["price"]}')
+    print(json.dumps(data, indent=2, sort_keys=True))
 
 if __name__ == "__main__":
     main()
